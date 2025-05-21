a23cf3b47b3896166aa6476dd6106fd6fae68257
Cristiano Ap Lazaro <cristianoalazaro@hotmail.com>
2025-05-21 20:57:37 -0300

    Init

---
+++ src/components/home/pizza-item.tsx
@@ -0,0 +1,30 @@
+import { Product } from "@/generated/prisma";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { decimalToMoney } from "@/lib/utils";
+
+type Props = {
+  data: Product;
+};
+
+export default function PizzaItem({ data }: Props) {
+  const handleAddToCart = () => {};
+
+  return (
+    <div className="text-sm bg-secondary p-4 rounded-md">
+      <Image
+        src={data.image}
+        alt={data.name}
+        width={200}
+        height={200}
+        className="w-full mb-3"
+      />
+      <div className="text-lg font-bold">{data.name}</div>
+      <div>{decimalToMoney(data.price)}</div>
+      <div className="truncate mb-3">{data.ingredients}</div>
+      <div className="text-center">
+        <Button onClick={handleAddToCart}>Adicionar ao carrinho</Button>
+      </div>
+    </div>
+  );
+}
